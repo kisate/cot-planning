72c200e32b67fdfcb7220c03f5a4ae4db1419953
kisate <dhkas@mail.ru>
2025-05-29 18:46:08 +0000

    new nodes

--- create_mystery.py
+++ create_mystery.py
@@ -11,7 +11,13 @@ from pathlib import Path
 # on: "craves"
 # holding: "pain"
 
-actions = {
+_actions = {
+    "mystery_2": {
+        "attack": "illuminate",
+        "succumb": "silence",
+        "overcome": "distill",
+        "feast": "divest"
+    },
     "mystery_3": {
         "attack": "tltezi",
         "succumb": "jchntg",
@@ -98,7 +104,14 @@ actions = {
     }
 }
 
-predicates = {
+_predicates = {
+    "mystery_2": {
+        "planet": "aura",
+        "province": "essence",
+        "harmony": "nexus",
+        "craves": "harmonizes",
+        "pain": "pulse"
+    },
     "mystery_3": {
         "planet": "oxtslo",
         "province": "adohre",
@@ -201,117 +214,118 @@ predicates = {
 
 domain = "mystery_16"
 
-predicates = predicates[domain]
-actions = actions[domain]
-
 predicate_order = [
     "pain",
     "harmony",
     "province",
     "planet",
     "craves"
-]    
+]  
 
-new_name = f"blocksworld_{domain}"
+def main(domain: str):
+    predicates = _predicates[domain]
+    actions = _actions[domain]
+    
 
-def replace_names(text: str, mapping: dict) -> str:
-    #lowercase
-    new_text = text
-    for key, value in mapping.items():
-        new_text = new_text.replace(key, value)
-    
-    for key, value in mapping.items():
-        new_text = new_text.replace(key.capitalize(), value.capitalize())
-        
-    return new_text
-        
+    new_name = f"blocksworld_{domain}"
 
-def create_config():
-    original_path = Path(
-        "configs/blocksworld_mystery.yaml"
-    )
-    
-    with open(original_path, "r") as f:
-        data = f.read()
+    def replace_names(text: str, mapping: dict) -> str:
+        #lowercase
+        new_text = text
+        for key, value in mapping.items():
+            new_text = new_text.replace(key, value)
         
-    new_path = Path(
-        f"configs/{new_name}.yaml"
-    )
-    
-    new_config = replace_names(data, actions)
-    new_config = replace_names(new_config, predicates)
-    new_config = new_config.replace("blocksworld_mystery", new_name)
-    
-    with open(new_path, "w") as f:
-        f.write(new_config)
-        
-def create_example_queries():
-    original_path = Path(
-        "example_query_prompts/blocksworld_mystery/standard.txt"
-    )
-    
-    with open(original_path, "r") as f:
-        data = f.read()
-        
-    new_path = Path(
-        f"example_query_prompts/{new_name}/standard.txt"
-    )
-    
-    new_config = replace_names(data, actions)
-    new_config = replace_names(new_config, predicates)
-    
-    new_path.parent.mkdir(parents=True, exist_ok=True)
-    
-    with open(new_path, "w") as f:
-        f.write(new_config)
-        
-def create_instances():
-    input_path = Path("instances/blocksworld_mystery/generated/")
+        for key, value in mapping.items():
+            new_text = new_text.replace(key.capitalize(), value.capitalize())
+            
+        return new_text
+            
 
-    def map_line(line: str):
-        for key in predicate_order:
-            if key in line:
-                line = line.replace(key, predicates[key])
-                return line
-        return line
+    def create_config():
+        original_path = Path(
+            "configs/blocksworld_mystery.yaml"
+        )
+        
+        with open(original_path, "r") as f:
+            data = f.read()
+            
+        new_path = Path(
+            f"configs/{new_name}.yaml"
+        )
+        
+        new_config = replace_names(data, actions)
+        new_config = replace_names(new_config, predicates)
+        new_config = new_config.replace("blocksworld_mystery", new_name)
+        
+        with open(new_path, "w") as f:
+            f.write(new_config)
+            
+    def create_example_queries():
+        original_path = Path(
+            "example_query_prompts/blocksworld_mystery/standard.txt"
+        )
+        
+        with open(original_path, "r") as f:
+            data = f.read()
+            
+        new_path = Path(
+            f"example_query_prompts/{new_name}/standard.txt"
+        )
+        
+        new_config = replace_names(data, actions)
+        new_config = replace_names(new_config, predicates)
+        
+        new_path.parent.mkdir(parents=True, exist_ok=True)
+        
+        with open(new_path, "w") as f:
+            f.write(new_config)
+            
+    def create_instances():
+        input_path = Path("instances/blocksworld_mystery/generated/")
 
-    for path in input_path.rglob("*.pddl"):
-        with open(path, "r") as f:
-            data = f.readlines()
+        def map_line(line: str):
+            for key in predicate_order:
+                if key in line:
+                    line = line.replace(key, predicates[key])
+                    return line
+            return line
+
+        for path in input_path.rglob("*.pddl"):
+            with open(path, "r") as f:
+                data = f.readlines()
+            
+            path = str(path).replace("blocksworld_mystery", new_name)
+            
+            Path(path).parent.mkdir(parents=True, exist_ok=True)
+            
+            with open(path, "w") as f:
+                for line in data:
+                    f.write(map_line(line))
+                    
+                    
+    def create_pddl():
+        original_path = Path(
+            "instances/blocksworld_mystery/domain.pddl"
+        )
         
-        path = str(path).replace("blocksworld_mystery", new_name)
+        with open(original_path, "r") as f:
+            data = f.read()
+            
+        new_data = replace_names(data, actions)
+        new_data = replace_names(new_data, predicates)
         
-        Path(path).parent.mkdir(parents=True, exist_ok=True)
+        new_path = Path(
+            f"instances/{new_name}/domain.pddl"
+        )
         
-        with open(path, "w") as f:
-            for line in data:
-                f.write(map_line(line))
-                
-                
-def create_pddl():
-    original_path = Path(
-        "instances/blocksworld_mystery/domain.pddl"
-    )
-    
-    with open(original_path, "r") as f:
-        data = f.read()
-        
-    new_data = replace_names(data, actions)
-    new_data = replace_names(new_data, predicates)
-    
-    new_path = Path(
-        f"instances/{new_name}/domain.pddl"
-    )
-    
-    with open(new_path, "w") as f:
-        f.write(new_data)
-    
-        
-    
-if __name__ == "__main__":
+        with open(new_path, "w") as f:
+            f.write(new_data)
     create_config()
     create_example_queries()
     create_instances()
     create_pddl()
     
-    print("Done!")
+        
+if __name__ == "__main__":
+    for i in range(2, 16):
+        main(f"mystery_{i}")
--- eval.sh
+++ eval.sh
@@ -21,11 +21,11 @@ set +a
 # python3 response_evaluation.py -t standard -c blocksworld_mystery_7 -e qwq-32b-original-full
 # python3 response_evaluation.py -t standard -c blocksworld_mystery_7 -e qwq-32b-steered-full
 
-MYSTERY_NUMS=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
+MYSTERY_NUMS=(8 9 10 11 12 13 14 15)
 # MYSTERY_NUMS=(6 5 7)
 # MYSTERY_NUMS=(9 6 5 7 12 10 3 4 11 8)
 # MYSTERY_NUMS=(1 2 13 15 14)
-MYSTERY_NUMS=(1 2 13)
+# MYSTERY_NUMS=(1)
 
 for MYSTERY_NUM in "${MYSTERY_NUMS[@]}"; do
     # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-60-greedy
@@ -35,12 +35,22 @@ for MYSTERY_NUM in "${MYSTERY_NUMS[@]}"; do
     # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-60-greedy_0
     # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-60-greedy_1
     # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-60-greedy_2
-    python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-100-mean-triple-8-greedy_0
-    python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-100-mean-triple-8-greedy_1
-    python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-100-mean-triple-8-greedy_2
-    python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-1-il-100-mean-triple-8-greedy_0
-    python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-1-il-100-mean-triple-8-greedy_1
-    python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-1-il-100-mean-triple-8-greedy_2
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-2-l-35-fix-rescale-r
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-2-l-35-fix-rescale
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-2-l-44-fix-rescale-r
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-2-l-44-fix-rescale
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-10-l-35-fix-rescale-r
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-10-l-35-fix-rescale
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-10-l-44-fix-rescale-r
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-10-l-44-fix-rescale
+    python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-st-full-end-1000-2500-10-l-35-fix-rescale-r
+    python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-st-full-end-1000-2500-10-l-35-fix-rescale
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-10-l-35-fix-rescale-r
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-replaced-rp-full-300-1000-2500-10-l-35-fix-rescale
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-100-mean-triple-8-greedy_2
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-1-il-100-mean-triple-8-greedy_0
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-1-il-100-mean-triple-8-greedy_1
+    # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-1-il-100-mean-triple-8-greedy_2
     # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-60-greedy_1
     # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-steered-full-0-il-60-greedy_2
     # python3 response_evaluation.py -t standard -c blocksworld_mystery_${MYSTERY_NUM} -e qwq-32b-greedy
